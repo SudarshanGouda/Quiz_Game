25f47e76bc49c2d216a883d13fa27e738dd83bfd
ADMIN <sudarshanggouda@gmail.com>
2022-11-23 15:45:56 +0530

    shuffle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from data import question_data
 from Question_model import Question
 from Quiz_brain import QuizBrain
+import random
 
 question_bank=[]
 
@@ -10,7 +11,11 @@ for question in question_data:
     new_question=Question(question_text,question_ans)
     question_bank.append(new_question)
 
+random.shuffle(question_bank)
 quiz=QuizBrain(question_bank)
 
 while quiz.still_have_question():
     quiz.next_question()
+
+print('Game is over')
+print(f'your score is {quiz.score}/12')
